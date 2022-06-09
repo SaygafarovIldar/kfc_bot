1cf3f69b4287e88066b13dea23c60b56ab22721a
Ildar <sayildar17@gmail.com>
2022-06-09 11:13:13 +0300

    fixed config data

--- config.py
+++ config.py
@@ -11,9 +11,4 @@ DATA_DIR_PATH = r"D:\ildar\python-training\data"
 TOKEN = os.getenv("TOKEN")
 
 # DB
-IP = os.getenv("IP")
-PG_USER = os.getenv("PG_USER")
-PG_PASSWORD = os.getenv("PG_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-
-DB_URL = f"postgresql://{PG_USER}:{PG_PASSWORD}@{IP}/{DB_NAME}"
+DB_URL = os.getenv("DATABASE_URL")
